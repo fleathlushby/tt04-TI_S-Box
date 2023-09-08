0620cfc9dc8a4b079e46cf7cdfa8dcf081ab2745
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 19:08:43 +0530

    Update tb.v

--- src/tb.v
+++ src/tb.v
@@ -1,53 +1,44 @@
+`default_nettype none
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.09.2023 13:03:45
-// Design Name: 
-// Module Name: test_wrapper
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module test_wrapper;
+module tb();
 
+// this part dumps the trace to a vcd file that can be viewed with GTKWave
+initial begin
+    $dumpfile ("tb.vcd");
+    $dumpvars (0, tb);
+    #1;
+end
+
+wire [7:0] uo_out;
+wire [7:0] ui_in;
+wire [7:0] uio_out;
+wire [7:0] uio_in;
+wire [7:0] uio_oe;
+wire clk;
+wire rst_n;
+wire ena;
+    
 reg[7:0] in;
 reg in_load,clk,rst;
 wire out_ready;
 wire[7:0] out;
 
-topmod dut(in, in_load, clk, rst, out_ready, out);
-
-initial
-    begin
-        clk<=1'd0;
-        rst<=1'd1;
-        in_load<=1'd0;
-    end
-
-always #10 clk=~clk;
-
-initial 
-    begin
-        #30 rst<=1'd0;
-        in_load<=1'd1;
-        in<=8'h63 ^ 8'h4 ^ 8'h5;
-        #20 in<=8'h4;
-        #20 in<=8'h5;
-        #20 in<=8'hf8;
-        #20 in<=8'h95;
-        #20 in_load<=1'd0;
-    end
+tt_um_ti_sbox tt_um_ti_sbox(
+    `ifdef GL_TEST
+    // include power ports for the Gate Level test
+        .VPWR( 1'b1),
+        .VGND( 1'b0),
+    `endif
+    .ui_in      (ui_in),    // Dedicated inputs
+    .uo_out     (uo_out),   // Dedicated outputs
+    .uio_in     (uio_in),   // IOs: Input path
+    .uio_out    (uio_out),  // IOs: Output path
+    .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+    .ena        (ena),      // enable - goes high when design is selected
+    .clk        (clk),      // clock
+    .rst_n      (rst_n)     // not reset
+        );
 
 endmodule
