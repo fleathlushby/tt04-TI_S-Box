9f991be8b944020d6d35290adfd78d5114e88a33
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 23:57:37 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -13,10 +13,10 @@ async def test_ti_sbox(dut):
 
     dut._log.info("reset")
     dut.rst_n.value = 0
-    dut.ena.value = 1
+    dut.ena.value = 0
     await ClockCycles(dut.clk, 120)
     dut.rst_n.value = 1
-    dut.ena.value = 0
+    dut.ena.value = 1
     # await ClockCycles(dut.clk, 100)
     # dut.ena.value = 1
     
