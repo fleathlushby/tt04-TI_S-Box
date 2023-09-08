08e4d34f04256f9c73f20d3219c64848ff0600ef
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 19:38:05 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,28 +2,26 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-# input_A = [0,1,2,3,4,5,6,7,5,6]
-# input_B = [7,6,5,4,3,2,1,0,5,7]
-# input_Cin = 0
-Sum = [7,7,7,7,7,7,7,7,10,13]
+input = [0x62, 0x04, 0x05, 0xf8, 0x95]
+output = [0x0, 0x0, 0x55, 0xa2, 0x0c]
 
 @cocotb.test()
-async def test_rca(dut):
+async def test_ti_sbox(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     dut._log.info("reset")
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ena.value = 1
+    await ClockCycles(dut.clk, 120)
     dut.rst_n.value = 1
+    dut.ena.value = 0
 
-    dut._log.info("check 10 inputs")
-    for i in range(10):
+    dut._log.info("check 5 inputs")
+    for i in range(5):
         dut._log.info("check input {}".format(i))
-        await ClockCycles(dut.clk, 1000)
-        # dut.ui_in[6:4].value = 0b101
-        # dut.ui_in[3:1].value = 0b010
-        dut.ui_in.value = 0b00111111
-        # assert int(dut.uo_out.value) == Sum[i]
+        await ClockCycles(dut.clk, 20)
+        dut.ui_in.value = input[i]
+        # assert int(dut.uo_out.value) == output[i]
         assert dut.uio_oe == 0xFF
