8a7652026c748915587759e41b95dde800950cb2
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-09 00:04:42 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -13,17 +13,17 @@ async def test_ti_sbox(dut):
 
     dut._log.info("reset")
     dut.rst_n.value = 0
-    dut.ena.value = 0
-    await ClockCycles(dut.clk, 120)
-    dut.rst_n.value = 1
     dut.ena.value = 1
+    await ClockCycles(dut.clk, 110)
+    dut.rst_n.value = 1
+    dut.ena.value = 0
     # await ClockCycles(dut.clk, 100)
     # dut.ena.value = 1
     
     dut._log.info("check 5 inputs")
     for i in range(5):
         dut._log.info("check input {}".format(i))
-        await ClockCycles(dut.clk, 110)
+        await ClockCycles(dut.clk, 20)
         dut.ui_in.value = input[i]
     await ClockCycles(dut.clk, 170)
     for i in range(10):
