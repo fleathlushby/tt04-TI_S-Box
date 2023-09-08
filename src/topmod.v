9840dcae6bf68432c71bd83c67b5cfa5c73d5c4f
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 18:58:30 +0530

    Update topmod.v

--- src/topmod.v
+++ src/topmod.v
@@ -1,37 +1,31 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.09.2023 10:29:28
-// Design Name: 
-// Module Name: topmod
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:0
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`default_nettype none
 
+module tt_um_ti_sbox(
+    input wire [7:0] ui_in, 
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    output wire [7:0] uo_out,
+    input wire clk, 
+    input wire rst_n, 
+    input wire ena);
 
-module topmod(input[7:0] in, input in_load, input clk, input rst, output reg out_ready, output reg[7:0] out);
-
+wire reset = ! rst_n;
+wire in_load;
+assign in_load = ena;
+assign uio_oe = 8'b11111111;   // use bidirectionals as outputs
+assign uio_out[0] = out_ready;
+assign uio_out[7:1] = 7'b0000000;
 reg[39:0] in_reg;
 reg[1:0] count,out_count;
 wire[7:0] out1,out2,out3;
 wire[7:0] sbox_out;
 
-sbox_ti uut(.in1(in_reg[39:32]), .in2(in_reg[31:24]), .in3(in_reg[23:16]), .R0(in_reg[15:8]), .R1(in_reg[7:0]), .clk(clk), .rst(rst), .out1(out1), .out2(out2), .out3(out3));
+sbox_ti uut(.in1(in_reg[39:32]), .in2(in_reg[31:24]), .in3(in_reg[23:16]), .R0(in_reg[15:8]), .R1(in_reg[7:0]), .clk(clk), .rst(reset), .out1(out1), .out2(out2), .out3(out3));
 
 always@(posedge clk)
     begin
-        if(rst)
+        if(reset)
             count<=2'd0;
         else if(in_load==1'd0 && count<2'd2)
             count<=count+1'd1;
@@ -41,7 +35,7 @@ always@(posedge clk)
 
 always@(posedge clk)
     begin
-        if(rst)
+        if(reset)
             out_ready<=1'd0;
         else if(count==2'd1)
             out_ready<=1'd1;
@@ -51,7 +45,7 @@ always@(posedge clk)
 
 always@(posedge clk)
     begin
-        if(rst)
+        if(reset)
             begin
                 in_reg=40'd0;
             end
@@ -68,7 +62,7 @@ always@(posedge clk)
     
 always@(posedge clk)
     begin
-        if(rst)
+        if(reset)
             out_count<=8'd0;
         else if(out_ready==1'd1 && out_count<2'd3)
             begin
@@ -80,15 +74,15 @@ always@(posedge clk)
     
 always@(posedge clk)
     begin
-        if(rst)
-            out<=8'd0;
+        if(reset)
+            uo_out<=8'd0;
         else if(out_count==2'd1)
-            out<=out1;
+            uo_out<=out1;
         else if(out_count==2'd2)
-            out<=out2;
+            uo_out<=out2;
         else if(out_count==2'd3)
-            out<=out3;
+            uo_out<=out3;
         else
-            out<=out;
+            uo_out<=uo_out;
     end 
 endmodule
