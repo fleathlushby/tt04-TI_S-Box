251827e0901f92c73166db5b57b563a74bb9e486
Tishya Sharma Sarkar <105290202+fleathlushby@users.noreply.github.com>
2023-09-08 22:44:45 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
-input = [0x95]
+input = [0x62, 0x04, 0x05, 0xf8, 0x95]
 output = [0x0, 0x0, 0x55, 0xa2, 0x0c]
 
 @cocotb.test()
@@ -23,7 +23,7 @@ async def test_ti_sbox(dut):
     dut._log.info("check 5 inputs")
     for i in range(5):
         dut._log.info("check input {}".format(i))
-        dut.ui_in.value = input[0]
+        dut.ui_in.value = input[i]
         await ClockCycles(dut.clk, 20)
         assert int(dut.uo_out.value) == output[i]
         assert dut.uio_oe == 0xFF
